982c06502ee38a3fbb2695d78a63bfdf88e38b28
Phelippe <phelippe.gabriel.7@gmail.com>
2024-02-21 13:54:39 -0300

    commit 21/02/2024

--- server/index.js
+++ server/index.js
@@ -1,1 +1,6 @@
 const express = require('express')
+const app = express()
+const server = require('http').createServer(app)
+const io = require("socket.io")
+
+server.listen(9000, () => console.log('Server running...'))
